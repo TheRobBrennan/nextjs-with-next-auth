9a2b2aac84a1fb168d50949342db05d0f5af8093
Rob Brennan <rob@therobbrennan.com>
2023-05-21 01:41:47 -0700

    Use the app router to define a new test page at /test

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+// The root layout replaces the pages/_app.tsx and pages/_document.tsx files
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Welcome to Next.js',
+}
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
